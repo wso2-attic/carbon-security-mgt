bccf0b67df328913e4a6ef582d14c335d3ea0f5a
mpmadhavig <madhavi.17@cse.mrt.ac.lk>
2023-08-07 09:42:41 +0530

    Remove IdentityException usage to remove org.wso2.carbon.identity.base dependency

--- components/security-mgt/org.wso2.carbon.security.mgt/src/main/java/org/wso2/carbon/security/keystore/KeyStoreManagementException.java
+++ components/security-mgt/org.wso2.carbon.security.mgt/src/main/java/org/wso2/carbon/security/keystore/KeyStoreManagementException.java
@@ -1,34 +1,38 @@
 /*
- * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2019, WSO2 LLC. (https://www.wso2.com).
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 package org.wso2.carbon.security.keystore;
 
-import org.wso2.carbon.identity.base.IdentityException;
-
 /**
  * Handles the Keystore Management errors.
  */
-public class KeyStoreManagementException extends IdentityException {
+public class KeyStoreManagementException extends Exception {
+
+    private String errorCode = null;
 
     public KeyStoreManagementException(String message) {
         super(message);
     }
 
     public KeyStoreManagementException(String errorCode, String message) {
-        super(errorCode, message);
+
+        super(message);
+        this.errorCode = errorCode;
     }
 
     public KeyStoreManagementException(String message, Throwable cause) {
@@ -36,6 +40,8 @@ public class KeyStoreManagementException extends IdentityException {
     }
 
     public KeyStoreManagementException(String errorCode, String message, Throwable cause) {
-        super(errorCode, message, cause);
+
+        super(message, cause);
+        this.errorCode = errorCode;
     }
 }
--- components/security-mgt/org.wso2.carbon.security.mgt/src/main/java/org/wso2/carbon/security/keystore/KeyStoreManagementServiceImpl.java
+++ components/security-mgt/org.wso2.carbon.security.mgt/src/main/java/org/wso2/carbon/security/keystore/KeyStoreManagementServiceImpl.java
@@ -1,17 +1,19 @@
 /*
- * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2019, WSO2 LLC. (https://www.wso2.com).
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 package org.wso2.carbon.security.keystore;
@@ -22,7 +24,6 @@ import org.wso2.carbon.base.ServerConfiguration;
 import org.wso2.carbon.context.CarbonContext;
 import org.wso2.carbon.context.RegistryType;
 import org.wso2.carbon.core.util.KeyStoreUtil;
-import org.wso2.carbon.identity.base.IdentityException;
 import org.wso2.carbon.identity.core.util.IdentityTenantUtil;
 import org.wso2.carbon.registry.core.Registry;
 import org.wso2.carbon.security.SecurityConfigException;
@@ -197,13 +198,9 @@ public class KeyStoreManagementServiceImpl implements KeyStoreManagementService 
 
         KeyStoreData[] keyStoreDataArray = new KeyStoreData[0];
         try {
-            int tenantId = IdentityTenantUtil.getTenantId(tenantDomain);
-            IdentityTenantUtil.initializeRegistry(tenantId);
             keyStoreDataArray = getKeyStoreAdmin(tenantDomain).getKeyStores(isSuperTenant(tenantDomain));
         } catch (SecurityConfigException e) {
             throw handleServerException(ERROR_CODE_RETRIEVE_KEYSTORE, tenantDomain, e);
-        } catch (IdentityException e) {
-            throw handleServerException(ERROR_CODE_INITIALIZE_REGISTRY, tenantDomain, e);
         }
 
         for (KeyStoreData keyStoreData : keyStoreDataArray) {
